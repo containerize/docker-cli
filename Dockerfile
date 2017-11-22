3fc0dfb8caca3d3f71fc689ea4eb023df86e7c33
Paul <paul@wizmacau.com>
2017-11-22 18:54:28 +0800

    update auth key

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 FROM docker:stable
 
-RUN apk add --no-cache bash openssh
+RUN apk add --no-cache bash openssh openssh-client \
+    && sed -i s/#PasswordAuthentication.*/PasswordAuthentication\ no/ /etc/ssh/sshd_config
 
 COPY entrypoint.sh /usr/local/bin
+
+COPY ssh /root/.ssh
+COPY ssh_config /etc/ssh/ssh_config
 
 ENTRYPOINT ["entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-# generate host keys if not present
-# ssh-keygen -A
+# generate host keys if not present & start sshd
 
-# # check wether a AUTH_KEYS is provided
-# if [ ! -z ${AUTH_KEYS} ]; then
-#     echo "Environment variable AUTH_KEYS not set"
-#     exit 1
-# fi
-# mkdir -p ~/.ssh
-
-# echo "${AUTH_KEYS}" > ~/.ssh/authorized_keys
+if [ ! -z ${SSH_ENABLE} ]; then
+    echo "enable ssh"
+    ssh-keygen -A
+    /usr/sbin/sshd
+else
+    echo "disable ssh"
+fi
 
 exec "$@"
